65b67ba629cd3d86da81cffc862d77c732917f8a
yakob-abada <yakob.abada@gmail.com>
2023-07-01 20:57:21 +0100

    Add coverage for mismatch passeword

--- go/app/application/user_handler_test.go
+++ go/app/application/user_handler_test.go
@@ -153,6 +153,47 @@ func TestLogin(t *testing.T) {
 		sut.Login(c)
 	})
 
+	t.Run("failed password", func(t *testing.T) {
+		var mockUserRepository = &repository.MockUserRepository{}
+		var mockErrorResponse = &service.MockErrorResponse{}
+		var mockValidate = &MockValidate{}
+		var mockUserAuthorization = &service.MockUserAuthoriztion{}
+		var mockAppCrypto = &service.MockAppCrypto{}
+
+		user := entity.User{
+			Id:       1,
+			Username: "username",
+			Password: "hashedPassword",
+		}
+		userAuth := service.AuthUser{
+			Username: "username",
+			Password: "password",
+		}
+
+		mockValidate.On("Struct", userAuth).Return(nil)
+		mockUserAuthorization.On("GenerateToken", user.Username, user.Id).Return(nil, fmt.Errorf("failed to generate JWT token"))
+		mockUserRepository.On("FindByUsername", "username").Return(&user, nil)
+		mockAppCrypto.On("CompareHashAndPassword", []byte("hashedPassword"), []byte("password")).Return(fmt.Errorf("invalid auth"))
+		mockErrorResponse.On("GenerateResponse", service.NewUnauthorizedError("Invalid credentials")).Return(405, &model.ErrorResponse{Error: "Invalid credentials"}).Once()
+		gin.SetMode(gin.TestMode)
+
+		w := httptest.NewRecorder()
+		c, _ := gin.CreateTestContext(w)
+		jsonbytes, _ := json.Marshal(userAuth)
+
+		c.Request, _ = http.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonbytes))
+
+		sut := UserHandler{
+			Repository:           mockUserRepository,
+			ErrorResponseHandler: mockErrorResponse,
+			UserAuthorization:    mockUserAuthorization,
+			Validate:             mockValidate,
+			Encryption:           mockAppCrypto,
+		}
+
+		sut.Login(c)
+	})
+
 	t.Run("failed create token", func(t *testing.T) {
 		var mockUserRepository = &repository.MockUserRepository{}
 		var mockErrorResponse = &service.MockErrorResponse{}
