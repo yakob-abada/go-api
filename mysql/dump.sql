2bfae56bf2982b0269f487af706a479874a84270
yakob-abada <yakob.abada@gmail.com>
2023-07-02 22:56:55 +0100

    Update dump.sql file

--- mysql/dump.sql
+++ mysql/dump.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: application
 -- ------------------------------------------------------
--- Server version	5.6.51
+-- Server version	8.0.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `class`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `class` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `week_day` int(11) NOT NULL,
-  `duration` int(11) NOT NULL,
+  `week_day` int NOT NULL,
+  `duration` int NOT NULL,
   `time` time NOT NULL,
-  `max_participant` int(11) NOT NULL,
+  `max_participant` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -51,14 +51,14 @@ DROP TABLE IF EXISTS `session`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `session` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `is_full` tinyint(1) NOT NULL DEFAULT '0',
   `time` datetime NOT NULL,
-  `class_id` int(11) NOT NULL,
+  `class_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_class_session` (`class_id`),
   CONSTRAINT `FK_class_session` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +67,7 @@ CREATE TABLE `session` (
 
 LOCK TABLES `session` WRITE;
 /*!40000 ALTER TABLE `session` DISABLE KEYS */;
-INSERT INTO `session` VALUES (1,1,'2023-06-26 07:00:00',1),(2,0,'2023-06-27 09:00:00',2),(3,0,'2023-06-28 07:00:00',3);
+INSERT INTO `session` VALUES (1,1,'2023-06-26 07:00:00',1),(2,0,'2023-06-27 09:00:00',2),(3,0,'2023-06-28 07:00:00',3),(4,0,'2023-07-03 07:00:00',1),(5,0,'2023-07-04 09:00:00',2),(6,0,'2023-07-05 07:00:00',3);
 /*!40000 ALTER TABLE `session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,8 +79,8 @@ DROP TABLE IF EXISTS `session_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `session_user` (
-  `session_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `session_id` int NOT NULL,
+  `user_id` int NOT NULL,
   UNIQUE KEY `unique_index` (`session_id`,`user_id`),
   KEY `FK_session_user` (`user_id`),
   CONSTRAINT `FK_session_class` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`),
@@ -94,7 +94,7 @@ CREATE TABLE `session_user` (
 
 LOCK TABLES `session_user` WRITE;
 /*!40000 ALTER TABLE `session_user` DISABLE KEYS */;
-INSERT INTO `session_user` VALUES (1,1),(2,1),(3,1),(1,2),(1,3);
+INSERT INTO `session_user` VALUES (1,1),(4,1),(1,2),(1,3);
 /*!40000 ALTER TABLE `session_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,13 +106,14 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(100) NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `user_username_index` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -122,7 +123,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Jakob','Abada','yakob.abada','$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK',1),(2,'Sam','Spark','sspark','$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK',1),(3,'Abdallah','Abada','a.abada','$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK',1),(4,'Tom','Hanks','tomh','$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK',1);
+INSERT INTO `user` VALUES (1,'Jakob','Abada','yakob.abada','$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK',1),(2,'Sam','Spark','sspark','$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK',1),(3,'Abdallah','Abada','a.abada','$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK',1),(4,'Tom','Hanks','tomh','test',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -135,4 +136,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-26 14:30:37
+-- Dump completed on 2023-07-02 22:50:18
